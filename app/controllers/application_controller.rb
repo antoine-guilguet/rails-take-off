a25efca78553fef816837bc1158313eea88fc5f7
Antoine Guilguet <guilguet.antoine@gmail.com>
2017-03-18 17:28:46 +0100

    Add naming on User and fit it with Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-  #
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-  # end
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in)        << :first_name
+    devise_parameter_sanitizer.for(:account_update) << :first_name
+  end
 end
