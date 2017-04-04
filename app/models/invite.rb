ec3a86f357ef0c5bad7065819914c4cfebb6cbb4
Antoine Guilguet <guilguet.antoine@gmail.com>
2017-04-04 17:14:14 +0200

    Add uniqueness of invitation for trip_id and mail address, correct translations

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,11 +1,10 @@
 class Invite < ActiveRecord::Base
   belongs_to :trip
 
-  # validates :email, presence: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }
-  validates :trip_id, numericality: true
-  # validates :host_id, numericality: true
-  # validates :token, presence: true
-  validates :recipient_id, uniqueness: { scope: :trip_id }
+  validates :email, presence: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }
+  validates :trip_id, numericality: true, uniqueness: { scope: :email }
+  validates :host_id, numericality: true
+  validates :token, presence: true
 
   def generate_token
     self.token = Digest::SHA1.hexdigest([self.trip_id, Time.now, rand].join)
