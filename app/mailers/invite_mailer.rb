2b4134c147fa52318f289506f3e4c51b5270d0a9
Antoine Guilguet <guilguet.antoine@gmail.com>
2017-05-01 12:57:59 +1000

    Fix invitation link for prod

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -4,7 +4,7 @@ class InviteMailer < ApplicationMailer
     @email = invite.email
     @trip= Trip.find(invite.trip_id)
     @host = User.find(invite.host_id)
-    @url  = root_url + new_user_registration_path
+    @url  = "https://take-off-prod.herokuapp.com/" + new_user_registration_path
 
     mail(to: @email, subject: "Join the Trip")
   end
