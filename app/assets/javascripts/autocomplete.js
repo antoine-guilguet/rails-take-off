7a41ad7d6278962c1e07b273669455c2128e9978
Antoine Guilguet <guilguet.antoine@gmail.com>
2017-06-05 10:24:01 +0200

    Implement first steps autocomplete

---
+++ app/assets/javascripts/autocomplete.js
@@ -0,0 +1,16 @@
+function initializeAutocomplete(id) {
+    var element = document.getElementById(id);
+    if (element) {
+        var autocomplete = new google.maps.places.Autocomplete(element, { types: ['geocode'] });
+        google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+    }
+}
+
+function onPlaceChanged() {
+    var place = this.getPlace();
+    // console.log(place);  // Uncomment this line to view the full object returned by Google API.
+}
+
+google.maps.event.addDomListener(window, 'load', function() {
+    initializeAutocomplete('trip_destination');
+});
