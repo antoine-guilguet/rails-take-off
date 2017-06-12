eed0e24604948fca3dcbba391af3a327e8111dc9
Antoine Guilguet <guilguet.antoine@gmail.com>
2017-06-12 12:16:07 +0200

    Improve i18n details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    locale = params[:locale].to_s.strip.to_sym
+    I18n.locale = I18n.available_locales.include?(locale) ? locale : I18n.default_locale
   end
 end
