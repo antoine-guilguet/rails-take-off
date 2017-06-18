81d35b38dc043512e8a19131bd750d6e445e7cc2
Antoine Guilguet <guilguet.antoine@gmail.com>
2017-06-19 00:39:50 +0200

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
+puts "Destroying DB"
+
+User.destroy_all
+Trip.destroy_all
+Survey.destroy_all
+
+puts "Seeding users"
+
 user_1 = User.create(first_name: "Antoine", last_name: "Guilguet", email:"guilguet.antoine@gmail.com", password: "brausadent1")
 user_2 = User.create(first_name: "Antoine", last_name: "2", email:"antoine.guilguet@hec.edu", password: "allezlom")
+
+puts "Seeding trip"
 
 dam = Trip.create(name: "Dam with Bros", destination: "Amsterdam", created: false )
 participant_1 = TripParticipant.create(trip_id: dam.id, user_id: user_1.id)
