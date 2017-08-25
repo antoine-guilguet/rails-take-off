b97c71562dfcb850804e701fabf9a6647715e581
Antoine Guilguet <guilguet.antoine@gmail.com>
2017-08-25 19:18:23 +0200

    Fix problems with FB login

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
+  devise_scope :users do
+    get 'sign_in', to: 'users/sessions#new'
+  end
+
   root 'pages#homepage'
 
   resources :trips do
